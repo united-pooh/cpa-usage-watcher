ba723a98ec7a23b504440d9da01b6420f16cd3b7
united_pooh <united_pooh@icloud.com>
2026-04-27 11:38:44 +0800

    Initial Commit

---
+++ cpa-usage-watcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cpa-usage-watcher
+//
+//  Created by 小咸 on 2026/4/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cpa-usage-watcher/cpa_usage_watcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  cpa_usage_watcherApp.swift
+//  cpa-usage-watcher
+//
+//  Created by 小咸 on 2026/4/27.
+//
+
+import SwiftUI
+
+@main
+struct cpa_usage_watcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
